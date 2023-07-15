cda7b46e1cb956392aa5172a07897712f28a9373
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-07-15 21:59:52 +0530

    Leetcode  171

---
+++ text3.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int titleToNumber(std::string columnTitle) {
+        int ans = 0;
+        for (auto c : columnTitle) {
+            int x = c - 'A' + 1;
+            ans = ans * 26 + x;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::string columnTitle = "ZY";
+    int columnNumber = solution.titleToNumber(columnTitle);
+    printf("Column Number: %d\n", columnNumber);
+
+    return 0;
+}
