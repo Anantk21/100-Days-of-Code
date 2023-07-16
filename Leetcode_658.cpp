635260e83a4388a8236fd783e3fb27111dc160a2
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-07-16 22:34:17 +0530

    Leeetcode_658

---
+++ Leetcode_658.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& A, int k, int x) {
+        int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return vector<int>(A.begin() + left, A.begin() + left + k);
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int k = 3;
+    int x = 4;
+    
+    vector<int> result = solution.findClosestElements(nums, k, x);
+    
+    cout << "Closest Elements: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+
+
+
+
+
