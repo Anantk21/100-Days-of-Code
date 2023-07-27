16062891e29f51b2c5f3b341d477ae3e227b6c4f
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-07-27 20:58:02 +0530

    leetcode_869updated

--- leetcode_869.cpp
+++ leetcode_869.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
+
 using namespace std;
 
 class Solution {
 public:
-    bool reorderedPowerOf2(int n) {
-        // Provided Solution class implementation here
-        // ...
+    bool reorderedPowerOf2(int n)
+    {
+        string nn = to_string(n);
+        
+        sort(nn.begin(), nn.end());
+        
+        for (int i = 0; i < 32; i++)
+        {
+            string pp = to_string(1 << i);
+            
+            sort(pp.begin(), pp.end());
+            
+            if (pp == nn)
+                return true;
+        }
+        
+        return false;
     }
 };
 
 void testSolution() {
     Solution sol;
-    int n = 46; // Replace this with any other number to test
+    int n = 33; 
 
     cout << "Checking if there is a power of 2 that has the same digits as " << n << " ... ";
     if (sol.reorderedPowerOf2(n))
