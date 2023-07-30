e02abd048dc22db5396f5e39cfcaaeee67fc6da0
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-07-30 23:16:16 +0530

    leetcode_350

---
+++ leetcode_350.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    // Test the intersect function
+    Solution solution;
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+    vector<int> result = solution.intersect(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
