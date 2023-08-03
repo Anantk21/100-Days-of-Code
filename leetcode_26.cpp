afad03f87cd977983aee845bdc0fe091d034c21c
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-08-03 14:52:00 +0530

    leetcode_26

---
+++ leetcode_26.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        int result = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[result] = nums[i];
+                ++result;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    // Example usage:
+    std::vector<int> nums = {1, 1, 2, 2, 3, 4, 5, 5, 5, 6};
+    Solution solution;
+    int newLength = solution.removeDuplicates(nums);
+
+    // Print the modified array and its length after removing duplicates
+    std::cout << "Modified Array: ";
+    for (int i = 0; i < newLength; ++i) {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "New Length: " << newLength << std::endl;
+
+    return 0;
+}
