24e5e3c4a8cada2acdd17e85ed60e8dad8ced3db
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-08-03 14:52:34 +0530

    leetcode_26

--- leetcode_26.cpp
+++ leetcode_26.cpp
@@ -18,7 +18,7 @@ public:
 
 int main() {
     // Example usage:
-    std::vector<int> nums = {1, 1, 2, 2, 3, 4, 5, 5, 5, 6};
+    std::vector<int> nums = {1, 1, 2, 2, 3, 4, 5, 5, 5, 6,6,8};
     Solution solution;
     int newLength = solution.removeDuplicates(nums);
 
