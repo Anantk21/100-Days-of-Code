453c0cb3747cd6589f5558cefee31d1efc0ac887
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-09-04 16:51:19 +0530

    leetcode_new

---
+++ letcode_new.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+    #include <vector>
+    #include <algorithm>
+    #include <numeric>
+    
+    using namespace std;
+    
+    class Solution {
+    public:
+        int minMoves(vector<int>& nums) {
+            return accumulate(begin(nums), end(nums), 0) - ((int)nums.size() * (int)*min_element(begin(nums), end(nums)));
+        }
+    };
+    
+    int main() {
+        // Test the Solution class
+        vector<int> nums = {1,2,3,4,5};
+        Solution solution;
+        int minMovesResult = solution.minMoves(nums);
+        cout << "Minimum Moves: " << minMovesResult << endl;
+    
+        return 0;
+    }
+    
