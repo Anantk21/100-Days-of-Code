ec58e6b327bd7fc23b44d58f588ee5535721b2ae
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-11-26 00:17:36 +0530

    little change in leetcode 1838

--- leetcode1838.cpp
+++ leetcode1838.cpp
@@ -9,7 +9,7 @@ public:
     int maxFrequency(vector<int>& nums, int k) {
         // ... (your provided code)
         sort(nums.begin(), nums.end());
-        long int i = 0, j = 0, sum = 0, l = 0, tsum = 0;
+        long int i = 0, j = 0, sum = 0, l = 0;
         for (i = 0; i < nums.size(); i++) {
             sum += nums[i];
             while ((i - j + 1) * nums[i] - sum > k) {
