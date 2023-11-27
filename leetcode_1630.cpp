465bbf59a9702ad6e7c01f14f1f5e54aadb400a5
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-11-28 01:01:37 +0530

    leetcode_1630

---
+++ leetcode_1630.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool> ans;
+        vector<int> temp;
+
+        for (int i = 0; i < l.size(); i++) {
+            int s = l[i];
+            int e = r[i] + 1;
+            temp = nums;
+            sort(temp.begin() + s, temp.begin() + e);
+            bool isArithmetic = true;
+            for (int j = s; j < e - 2; j++) {
+                if (temp[j + 1] - temp[j] != temp[j + 2] - temp[j + 1]) {
+                    isArithmetic = false;
+                    break;
+                }
+            }
+            ans.push_back(isArithmetic);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    // Test the Solution class
+    Solution solution;
+
+    // Example usage
+    vector<int> nums = {4, 6, 5, 9, 3, 7};
+    vector<int> l = {0, 0, 2};
+    vector<int> r = {2, 3, 5};
+
+    vector<bool> result = solution.checkArithmeticSubarrays(nums, l, r);
+
+    // Display the result
+    cout << "Result: ";
+    for (bool val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
