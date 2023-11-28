b6d7a01abad648b69878c0ade9a1cd7a6012d662
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-11-29 00:50:33 +0530

    modified

--- leetcode_2038.cpp
+++ leetcode_2038.cpp
@@ -1,29 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
         
-        int r_sum,t_sum;
+        int r_sum, t_sum;
         int m = rolls.size();
-     
         
-        int temp_sum = accumulate(rolls.begin(),rolls.end(),0);
+        int temp_sum = accumulate(rolls.begin(), rolls.end(), 0);
         
-        t_sum = mean * (m+n);
-        r_sum = t_sum-temp_sum;
+        t_sum = mean * (m + n);
+        r_sum = t_sum - temp_sum;
         
-        if(r_sum>6*n || r_sum <n){
+        if (r_sum > 6 * n || r_sum < n) {
             return {};
         }
         
-        int a = r_sum/n;
-        int b = r_sum%n;
+        int a = r_sum / n;
+        int b = r_sum % n;
         
-        vector<int> ans(n,a);
+        vector<int> ans(n, a);
         
-        for(int i=0;i<b;i++){
+        for (int i = 0; i < b; i++) {
             ans[i]++;
         }
         return ans;
-        
     }
 };
+
+int main() {
+    // Example usage
+    vector<int> rolls = {1, 2, 3, 4};
+    int mean = 4;
+    int n = 2;
+
+    Solution solution;
+    vector<int> result = solution.missingRolls(rolls, mean, n);
+
+    // Print the result
+    cout << "Result: [";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+        if (i < result.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
