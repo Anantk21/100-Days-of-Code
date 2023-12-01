e04c6383b09466ca6bbeca06368eca481e01df15
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-12-01 22:38:06 +0530

    leetcode_2592

---
+++ leetcode_2592.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximizeGreatness(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        
+        int i = 0, j = 0, cnt = 0;
+        
+        while (j < nums.size()) {
+            if (nums[i] >= nums[j]) {
+                j++;
+            } else if (nums[i] < nums[j]) {
+                cnt++;
+                i++;
+                j++;
+            }
+        }
+        return cnt;
+    }
+};
+
+int main() {
+    // Example usage
+    std::vector<int> exampleVector = {3, 1, 4, 2, 2};
+    Solution solution;
+    int result = solution.maximizeGreatness(exampleVector);
+    
+    // Print the result (you can modify this based on your requirements)
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
