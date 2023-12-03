fa0870721113596e3979a9ce0d97488fcc344171
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-12-03 22:45:04 +0530

    leetcode_2124

---
+++ leetcode_2124.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    bool checkString(std::string s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s[i] == 'b' && s[i + 1] == 'a') {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Example usage
+    std::string inputString = "example_string";
+    bool result = solution.checkString(inputString);
+
+    // Print the result
+    std::cout << "Result: " << (result ? "true" : "false") << std::endl;
+
+    return 0;
+}
