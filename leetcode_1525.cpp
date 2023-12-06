d44711b34eda5d84c3fa555f0f87595604e775ac
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-12-06 23:49:01 +0530

    leetcode_1525

---
+++ leetcode_1525.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSplits(string s) {
+        int n = s.size(), ans = 0;
+        unordered_map<char, int> left, right;
+
+        for (int i = 0; i < n; i++)
+            right[s[i]]++;
+
+        for (int i = 0; i < n; i++) {
+            left[s[i]]++;
+            right[s[i]]--;
+
+            if (right[s[i]] == 0)
+                right.erase(s[i]);
+
+            if (left.size() == right.size())
+                ans++;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    // Example usage
+    Solution solution;
+    string input = "aacaba";
+    int result = solution.numSplits(input);
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
