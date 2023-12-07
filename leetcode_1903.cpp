257c8cd4b343a30b418e1d50f11105c660308e54
Anant Krishna <110186229+Anantk21@users.noreply.github.com>
2023-12-07 16:27:47 +0530

    leetcode_1903

---
+++ leetcode_1903.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    std::string largestOddNumber(std::string num) {
+        int i = num.length() - 1;
+
+        while (i >= 0) {
+            int n = num[i];
+            if (n % 2 == 1) {
+                return num.substr(0, i + 1);
+            } else {
+                i--;
+            }
+        }
+        return "";
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Example usage
+    std::string num = "123456";
+    std::string result = solution.largestOddNumber(num);
+
+    // Displaying the result
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
